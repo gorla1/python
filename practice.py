0899a4bc41ca66c630442a00328e4e2fb3beeece
gorla1 <gorladineshyadav1@gmail.com>
2021-04-06 16:39:23 +0530

    buttons

--- practice.py
+++ practice.py
@@ -1,12 +1,13 @@
-'''if 5>2:print("hello world")
-
-s = "dinesh"
-print(s[4:6])'''
-
-f=open("myfile.txt" ,"w")
-f.write("Hellow World")
-f.close()
-
-f=open("myfile.txt","r")
-print(f.read())
-
+even = []
+odd = []
+even_Sum = 0
+odd_Sum = 0
+n=int(input("enter the number of elements"))
+for i in range(0,n):
+         num=int(input())
+         if num % 2 == 0:even.append(num)
+         else:odd.append(num)
+for x in range(0,len(even)):even_Sum=even_Sum+even[x]
+for x in range(0,len(odd)):odd_Sum=odd_Sum+odd[x]
+total = even_Sum - odd_Sum
+print(even,odd,even_Sum,odd_Sum,total)
